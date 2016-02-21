bce187890da7cf631a9bd1fdd71ce4dce2489200
Vito Van <awesomevito@live.com>
2016-02-21 19:49:37 +0800

    add main args

--- jopy/jopy.py
+++ jopy/jopy.py
@@ -1,22 +1,28 @@
 from spider import zhaopin, neitui, lagou, woou
 from threading import Thread
+import sys
 
 def spider_thread(spider):
     spider.start_request()
 
-zhaopin_thread = Thread(target = spider_thread, args = (zhaopin.Spider('北京'),))
-lagou_thread = Thread(target = spider_thread, args = (lagou.Spider('北京'),))
-neitui_thread = Thread(target = spider_thread, args = (neitui.Spider('北京'),))
-woou_thread = Thread(target = spider_thread, args = (woou.Spider('北京'),))
+def main(city):
+    zhaopin_thread = Thread(target = spider_thread, args = (zhaopin.Spider(city),))
+    lagou_thread = Thread(target = spider_thread, args = (lagou.Spider(city),))
+    neitui_thread = Thread(target = spider_thread, args = (neitui.Spider(city),))
+    woou_thread = Thread(target = spider_thread, args = (woou.Spider(city),))
 
-zhaopin_thread.start()
-lagou_thread.start()
-neitui_thread.start()
-woou_thread.start()
+    zhaopin_thread.start()
+    lagou_thread.start()
+    neitui_thread.start()
+    woou_thread.start()
 
-zhaopin_thread.join()
-lagou_thread.join()
-neitui_thread.join()
-woou_thread.join()
+    zhaopin_thread.join()
+    lagou_thread.join()
+    neitui_thread.join()
+    woou_thread.join()
 
-print('All systems done.')
+    print('All systems done.')
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
